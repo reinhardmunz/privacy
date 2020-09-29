d3d5ea385c6b2474b37c4a8d21e8843bc31e32d7
Reinhard Munz <reinhardmunz@users.noreply.github.com>
2020-09-29 04:04:44 +0200

    removed previous change, tutorials is not a module, needs to be on pythonpath

--- tutorials/mnist_dpsgd_tutorial.py
+++ tutorials/mnist_dpsgd_tutorial.py
@@ -29,7 +29,6 @@ import tensorflow.compat.v1 as tf
 from tensorflow_privacy.privacy.analysis import compute_dp_sgd_privacy_lib
 from tensorflow_privacy.privacy.optimizers import dp_optimizer
 
-#from tutorials import mnist_dpsgd_tutorial_common as common
 import mnist_dpsgd_tutorial_common as common
 
 flags.DEFINE_boolean(
